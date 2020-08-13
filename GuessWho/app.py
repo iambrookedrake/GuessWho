d1053c503b1039eb13b3bdad512d26fb61d38fa0
Brooke Drake <iambrookedrake@gmail.com>
2020-08-12 21:42:16 -0600

    class

--- GuessWho/app.py
+++ GuessWho/app.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template, request
 from .model import db, User, Tweet
 from .twitter import add_user_tweepy, update_all_users
 from .predict import predict_user
+from dotenv import load_dotenv
+load_dotenv()
 
 def create_app():
     """Create and configure an instance of the Flask application"""
 
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:\\Users\\iambr\\Desktop\\Unit3\\GuessWho\\GuessWho\\GuessWho.sqlite"
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv('DATABASE_URL')#"sqlite:///C:\\Users\\iambr\\Desktop\\Unit3\\GuessWho\\GuessWho\\GuessWho.sqlite"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
